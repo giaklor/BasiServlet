25c270d073d96abc80737651310d6f5658310195
giaklor <giacomoannaloro92@gmail.com>
2014-06-11 17:02:34 +0200

    Script creazione tabelle

---
+++ sql/script.sql
@@ -0,0 +1,76 @@
+CREATE TABLE Tipo (
+	Denominazione VARCHAR(50) PRIMARY KEY,
+	Descrizione VARCHAR(200)
+);
+
+CREATE TABLE Iscritto (
+	Email VARCHAR(50) PRIMARY KEY,
+	Nome VARCHAR(30) NOT NULL,
+	Cognome VARCHAR(30) NOT NULL,
+	Data_nascita DATE,
+	Username VARCHAR(20) NOT NULL,
+	Password VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Istruttore (
+	Id_istruttore INTEGER PRIMARY KEY,
+	Nome VARCHAR(30) NOT NULL,
+	Cognome VARCHAR(30) NOT NULL,
+	Telefono VARCHAR(15),
+);
+
+CREATE TABLE Corso (
+	Id_corso INTEGER PRIMARY KEY,
+	Nome VARCHAR(30),
+	Data_inizio DATE,
+	Data_fine DATE NOT NULL,
+	Tipo_attivita VARCHAR(50),
+	Tipo_corso CHAR NOT NULL,
+	Istruttore_resp INTEGER NOT NULL,
+	UNIQUE(Nome, Data_inizio),
+	CHECK(Tipo_corso IN('s', 'm')),
+	FOREIGN KEY (Istruttore) REFERENCES Istruttore(Id_istruttore),
+	FOREIGN KEY (Tipo_attivita) REFERENCES Tipo(Denominazione)
+);
+
+CREATE TABLE Lezione (
+	Corso INTEGER,
+	Giorno INTEGER,
+	Ora_inizio TIME,
+	Ora_fine TIME NOT NULL,
+	PRIMARY KEY(Corso, Giorno, Ora_inizio),
+	FOREIGN KEY (Corso) REFERENCES Corso(Id_corso),
+	CHECK(Giorno >= 1 AND Giorno <= 7)
+);
+	
+CREATE TABLE Materiale_didattico (
+	Percorso VARCHAR(200) PRIMARY KEY,
+	Nome VARCHAR(30),
+	Tipo VARCHAR(15),
+	Formato VARCHAR(10)
+);
+
+CREATE TABLE Materiali_corso (
+	Corso INTEGER,
+	Materiale VARCHAR(200),
+	PRIMARY KEY (Corso, Materiale),
+	FOREIGN KEY (Corso) REFERENCES Corso(Id_corso),
+	FOREIGN KEY (Materiale) REFERENCES Materiale(Percorso)
+);
+
+CREATE TABLE Istruttori_Corsi (
+	Istruttore INTEGER,
+	Corso INTEGER
+	PRIMARY KEY (Istruttore, Corso),
+	FOREIGN KEY (Istruttore) REFERENCES Istruttore(Id_istruttore),
+	FOREIGN KEY (Corso) REFERENCES Corso(Id_corso)
+);
+
+CREATE TABLE Iscrizione (
+	Data_iscrizione DATE NOT NULL,
+	Iscritto VARCHAR(50),
+	Corso INTEGER,
+	PRIMARY KEY (Iscritto, Corso),
+	FOREIGN KEY (Iscritto) REFERENCES Iscritto(Email),
+	FOREIGN KEY (Corso) REFERENCES Corso(Id_corso)
+);
